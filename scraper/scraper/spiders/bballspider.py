566fb72a35ee5ab2788269acfb968b594853f378
mgirouard23 <Girouard23@matthews-mbp-6.wireless.dartmouth.edu>
2014-10-24 00:19:28 -0400

    Fixed scraping issues started CSV format

--- scraper/scraper/spiders/bballspider.py
+++ scraper/scraper/spiders/bballspider.py
@@ -1,17 +1,26 @@
 import scrapy
 
-
 class CurrySpider(scrapy.Spider):
     name = "curry"
     allowed_domains = ["http://www.basketball-reference.com/"]
     start_urls = []
     with open("spiders/players", 'r') as f:
-        [start_urls.append("http://www.basketball-reference.com" + line) for line in f]
+        for line in f.readlines():
+            s = "http://www.basketball-reference.com" + line
+            s = s[:-1]
+            start_urls.append(s) 
     start_urls = start_urls[:10]
+    print start_urls
+    
+
 
     def parse(self, response):
-        filename = 'stats'
+        filename = "stats"
         player_stats = response.xpath('//tr[@class="full_table"]/td/text()').extract()
+        print player_stats
         with open(filename, 'a') as f:
-            [f.write(data) for data in player_stats]
+            for data in player_stats:
+                f.write(data + ",")
             f.write("\n")
+
+    
