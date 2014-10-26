07ce0e547fad98be4264b547e427f7d9cdbed552
John Conley <JCC282@gmail.com>
2014-10-26 19:19:58 -0400

    Finish scraping code

--- scraper/scraper/spiders/bballspider.py
+++ scraper/scraper/spiders/bballspider.py
@@ -14,38 +14,70 @@ class CurrySpider(scrapy.Spider):
             s = s[:-1]
             start_urls.append(s)
 
-
-    # remove when appropriate
-    start_urls = start_urls[:1]
-    print start_urls
+    # start_urls = start_urls[:10]
 
     def parse(self, response):
         filename = "stats"
         player_id = response.url.split("/")[-1].split(".")[0]
-        name = response.xpath('//div[@id="info_box"]/h1/text()').extract()[0]
+        name = response.xpath('//div[@id="info_box"]//h1/text()').extract()[0]
         table = '//table[@id="per_minute"]/tbody'
-        row = '/tr[@class="full_table"]'
-        num_rows = len(response.xpath(table).css('tr').xpath('//td[1]'))
-
-        def column(k):
-            return table + row + '/td[' + str(k) + ']/text()'
+        rows = response.xpath(table).css('tr')
+        fields = ['id',
+                  'name',
+                  'season',
+                  'age',
+                  'team',
+                  'league',
+                  'position',
+                  'games',
+                  'games_started',
+                  'minutes_played',
+                  'field_goals',
+                  'field_goal_attempts',
+                  'field_goal_percentage',
+                  'three_point_fg',
+                  'three_point_fga',
+                  'three_point_fgp',
+                  'two_point_fg',
+                  'two_point_fga',
+                  'two_point_fgp',
+                  'free_throws',
+                  'free_throw_attempts',
+                  'free_throw_percentage',
+                  'off_rebounds',
+                  'def_rebounds',
+                  'tot_rebounds',
+                  'assists',
+                  'steals',
+                  'blocks',
+                  'turnovers',
+                  'fouls',
+                  'points']
+        scraping_fields = fields[2:]
 
         player_stats = []
-        for i in range(num_rows ):
+        for row in rows:
             l = ItemLoader(item=Player(), response=response)
             l.add_value('id', player_id)
             l.add_value('name', name)
-            # l.add_xpath('season', )
 
-            # add all fields here
+            for j, f in enumerate(scraping_fields):
+                # get text in ith row, jth column of the table
+                val = row.css('td')[j].xpath('text()').extract()
+                if len(val) < 1:
+                    val = row.css('td')[j].xpath('a/text()').extract()
 
-            l.add_xpath('age', column(2))
+                # add value to the ItemLoader
+                if len(val) < 1:
+                    l.add_value(f, '')
+                else:
+                    l.add_value(f, val[0])
+
             player_stats.append(l.load_item())
 
         with open(filename, 'a') as f:
             for item in player_stats:
-                # this prints the fields in alphabetical order, not in the order we define them
-                [f.write(data[1][0] + ',') for data in item.items()]
+                # get the value of the fields for each individual item
+                vals = [item[field][0] for field in fields]
+                f.write(','.join(vals))
                 f.write('\n')
-
-        # print "num_rows = " + str(num_rows)
