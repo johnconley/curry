b436a4b94efa3a553612e0209c7d6eaa3f320328
John Conley <JCC282@gmail.com>
2014-11-15 22:44:13 -0500

    Implement gradient boosting decision trees

---
+++ gbdt.m
@@ -0,0 +1,32 @@
+function f = gbdt(X, Y)
+
+M = 10;
+delta = 1;
+avg_Y = mean(Y);
+
+% initialize model with a constant value
+hs = cell(M,1);
+bs = zeros(M,1);
+hs{1} = 1;
+bs(1) = avg_Y;
+
+for i = 2:M
+    % compute pseudo-residuals
+    predictions = predict_gbdt(X, bs, hs, i-1);
+    residuals = Y - predictions;
+    
+    % train a decision tree on pseudo-residuals
+    % replace with our own decision tree calculator
+    h = fitrtree(X, residuals);
+    hs{i} = h;
+    
+    % find arg min_b huber_loss(Y,pred_Y)
+    b1 = bs(1:i-1);
+    b2 = bs(i+1:end);
+    g = @(b) huber_loss(Y, predict_gbdt(X, [b1;b;b2], hs, i), delta);
+    bs(i) = fmincon(g,1,[],[],[],[],0,20);
+end
+
+f = @(x) predict_gbdt(x, bs, hs);
+
+end
---
+++ predict_gbdt.m
@@ -0,0 +1,21 @@
+function Y = predict_gbdt(X, bs, hs, M)
+
+switch nargin
+    case 3
+        M = size(bs,1);
+end
+
+m = size(X,1);
+Y = zeros(m,1);
+for i = 1:m
+    x = X(i,:);
+    y = bs(1)*hs{1};
+    for j = 2:M
+        b = bs(j);
+        h = hs{j};
+        y = y + b*h.predict(x);
+    end
+    Y(i) = y;
+end
+
+end
