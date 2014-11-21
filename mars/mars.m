4289883bab35294b9c5033b03bd1a7a62bf34061
mgirouard23 <Girouard23@matthews-mbp-8.wireless.dartmouth.edu>
2014-11-21 15:34:15 -0500

    Removed mars.m because it is unecessary

--- mars/mars.m
+++
@@ -1,9 +0,0 @@
-function [knots, B, H] = mars(X, Y, max_terms, mode)
-% X: [m x n] matrix of training input data
-% Y: [m x 1] vector of training output data
-% max_terms: maximum numbers of terms in the model
-
-[knots, B, H] = forward_pass(X, Y, max_terms, mode);
-[knots, B]  = backward_pass(X, Y, knots, B);
-
-end
