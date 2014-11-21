d856e2e41b1cfa065b8b6dbed3eb51e0b93ce53c
mgirouard23 <Girouard23@matthews-mbp-8.wireless.dartmouth.edu>
2014-11-21 16:22:20 -0500

    Added to the README

--- mars/run_mars.m
+++ mars/run_mars.m
@@ -4,7 +4,7 @@ function run_mars()
 marsplot('pos', '2P%', 'per_poss_pos_1.csv', 20, 'Point Guard', 100);
 marsplot('pos', 'TRB', 'per_poss_pos_5.csv', 20, 'Center', 100);
 marsplot('pos', 'PTS', 'per_poss_pos_2.csv', 20, 'Shooting Guard', 100);
-marsplot('pos', 'AST', 'per_poss_pos_5.csv', 20, 'Center', 100);
+marsplot('pos', 'AST', 'per_poss_pos_1.csv', 20, 'Point Guard', 100);
 
 
 end
